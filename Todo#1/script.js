f2b9265fdfe87b48754b7fed8a9e2752c8d855a1
Beefsupreme21 <101227906+Beefsupreme21@users.noreply.github.com>
2022-06-19 18:58:17 -0600

    Add files via upload

---
+++ Todo#1/script.js
@@ -0,0 +1,18 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById("toDoContainer"); 
+let inputField = document.getElementById("text");
+let deleteButton = document.getElementById("delete-button");
+
+
+
+addToDoButton.addEventListener('click', function(){ 
+    let paragraph = document.createElement('ul'); 
+    paragraph.classList.add('paragraph-styling'); 
+    paragraph.innerText = inputField.value; 
+    toDoContainer.appendChild(paragraph); 
+    inputField.value = ""; 
+
+    deleteButton.addEventListener('click', function(){ 
+        toDoContainer.removeChild(paragraph);
+})
+})
