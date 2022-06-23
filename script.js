06c22c4f3e747f4611936e76a7e34952c523943f
Beefsupreme21 <beefsupreme21@hotmail.com>
2022-06-22 19:48:33 -0600

    Added individual delete button

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById("toDoContainer"); 
 let inputField = document.getElementById("text");
-let deleteButton = document.getElementById("delete-button");
+let clearButton = document.getElementById("clear-button");
 
-//test2
 
 addToDoButton.addEventListener('click', function(){ 
     let paragraph = document.createElement('ul'); 
     paragraph.classList.add('paragraph-styling'); 
     paragraph.innerText = inputField.value; 
+
+    if (inputField.value == "") {
+        alert ("Please fill out all boxes")
+      }
+      
+    else {
     toDoContainer.appendChild(paragraph); 
     inputField.value = ""; 
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button'); 
+    deleteButton.innerText = 'Delete';
+    deleteButton.style = 'margin-left: 120px';
+    paragraph.appendChild(deleteButton);
 
+    clearButton.addEventListener('click', function(){ 
+        toDoContainer.removeChild(paragraph);
+    })
     deleteButton.addEventListener('click', function(){ 
         toDoContainer.removeChild(paragraph);
-})
-})
+    })
+}})
